0c0cd2413e425d1dbda1e2aa4ed571e3a21a4007
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-06-29 02:53:30 -0700

    create variables for getelementbyID to pull from html

---
+++ script.js
@@ -0,0 +1,18 @@
+function renderPage() {
+    var inputEl = document.getElementById("city-input");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");4
+    var currentWindEl = document.getElementById("wind-speed");
+    var currentUVEl = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history");
+    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
+  
+    var APIKey = "257dd9a9e57952ff496d8ab275bb751a";
+ 
+  
