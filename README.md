22cb3d3a25cb1e3f458fccf2aa57e43af425768b
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-06-29 23:56:20 -0700

    Added keydown function for user to press enter, search history function to save and finished README final commit

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # Weather-API-Homework
+
+
+https://anotherarod.github.io/Weather-API-Homework/
+
+
+
+HTML(step 1):
+
+Lines 1-11: Set up standard HTML template along with fontawesome link and bootstrap. This is placed all within the head tag and with a title tag.
+<------------------------------------------------------------------------>
+Lines 13-14: We have a Navbar with styling and a H1 tag to display Weather Dashboard.
+<------------------------------------------------------------------------>
+Lines 17-31: A div tag with a class of "container" to wrap all the sunsequent rows for the remainding HTML.
+<------------------------------------------------------------------------>
+
+Lines 18-25: Within these rows we will have div tags with classes for styling, input tags with ids for elements we will call in the script.js file and buttons for the search bar and clear history input. we will also incorporate some font awesome touches.
+<------------------------------------------------------------------------>
+
+Lines 32-40: Here we have a class of columns with a couple rows. This space on the page will be used to hold the city and temperature with UVindex from the API calls.
+
+Now this was especially challenging. During the HTML validation process I encountered an error that just would not go aware because there was an img tag without a src= in the tag, however there was, it was just a placeholder.(example: src="placeholder"), I tried src=(""),src="//:0", and about:blank.
+
+Nothing worked, after freaking out and having an anxiety attack I gave it one last shot and found this src="data:," which allowed me to have a img tag with an empty src. #winning:)
+<------------------------------------------------------------------------>
+
+Lines 41-62: This part in the body is comprised of the divs and rows that will hold the temp, wind speed, humidity, and UVindex.
+<------------------------------------------------------------------------>
+
+Lines 63-77: I have placed a the final row with 5 classes of column to hold the 5-day forecast. 
+
+Lines 79-84 The final touch to the HTML was the script tag to link to my script.js file. A jquery link. To be honest, I am somewhat weak with Jquery and need more practice.  During the research process and me figuring out how I was even gonna approach this assignment I came across an interesting technology and after trying out a couple snippets of code I learned it was rather simple to use axios.get for requests instead of the $.ajax() function. I pulled the script tag link from this website https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/
+
+
+Really cool stuff!
+<------------------------------------------------------------------------>
+
+Javascript (Step 2) Generally I would say css is second but in this case not too much styling is necessary and alot can be done within the tages in the HTML.
+<------------------------------------------------------------------------>
+
+Line 1: I have created a renderpage function which I will complete at the end of the page.
+<------------------------------------------------------------------------>
+
+Lines 2-12: This area will hold the var global variables which i will call through the script.js.
+<------------------------------------------------------------------------>
+
+Line 12: A search history variable with a JSON parse to local storage.
+<------------------------------------------------------------------------>
+
+Line 13: A console.log for (searchHistory) and to check for errors.
+<------------------------------------------------------------------------>
+
+Moving forward the challenge for me specifically was doing multiple get requests. Thank god for th tutor that I signed up for because he helped me understand what I was missing and gave me confidence that I wasn't far off.
+
+After I did the first one, the rest kind of fell into place, albeit with a couple hurdles here and there.
+
+Next came a getWeather function with curly braces and within them were 3 main get requests. The main query URL, Forecasts, and UVIndex.
+
+Each of those were followed by console.log(response)
+<------------------------------------------------------------------------>
+
+Lines 91-99: I set up and event listener for my input element and search element with local storage.
+
+As part of this I used a "keydown" function to acknowledge when the user presses the "enter" button on the keyboard when they input a city to forecast.
+
+<------------------------------------------------------------------------>
+
+Lines 120-121: I made a function to convert kelvin to fahrenheit and used math.floor method to make sure the populated temp would be without decimals and rounded down to the nearest integer.
+
+Line 124: Lastly I created a render search history function to keep users past city forecast choices in the history coloumn under the search bar.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function renderPage() {
+    var APIKey = "257dd9a9e57952ff496d8ab275bb751a";
+    
     var inputElement = document.getElementById("city-input");
     var searchElement = document.getElementById("search-button");
     var clearElement = document.getElementById("clear-history");
@@ -11,137 +13,136 @@ function renderPage() {
     var historyElement = document.getElementById("history");
     var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
     console.log(searchHistory);
-       
- 
-     var APIKey = "257dd9a9e57952ff496d8ab275bb751a";
-   
+
+
+    
 
     function getWeather(cityName) {
 
-              var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-              axios.get(queryURL)
-              .then(function(response){
-                  console.log(response);
-  
-                  var currentDate = new Date(response.data.dt*1000);
-                  console.log(currentDate);
-                  var day = currentDate.getDate();
-                  var month = currentDate.getMonth() + 1;
-                  var year = currentDate.getFullYear();
-                  nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
-                  var weatherPic = response.data.weather[0].icon;
-                  currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-                  currentPicEl.setAttribute("alt",response.data.weather[0].description);
-                  tempElement.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
-                  humidityElement.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-                  windElement.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
-              var lat = response.data.coord.lat;
-              var lon = response.data.coord.lon;
-              
-              var UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-              axios.get(UVQueryURL)
-              .then(function(response){
-                  var UVIndex = document.createElement("span");
-                  UVIndex.setAttribute("class","badge badge-danger");
-                  UVIndex.innerHTML = response.data[0].value;
-                  uvIndexElement.innerHTML = "UV Index: ";
-                  uvIndexElement.append(UVIndex);
-              });
-              
-              var cityID = response.data.id;
-              var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
-              axios.get(forecastQueryURL)
-              .then(function(response){
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        axios.get(queryURL)
+            .then(function (action) {
+                console.log(action);
 
-                  console.log(response);
+                var currentDate = new Date(response.data.dt * 1000);
+                console.log(currentDate);
+                var day = currentDate.getDate();
+                var month = currentDate.getMonth() + 1;
+                var year = currentDate.getFullYear();
+                nameEl.innerHTML = action.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                var weatherPic = action.data.weather[0].icon;
+                currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                tempElement.innerHTML = "Temperature: " + k2f(action.data.main.temp) + " &#176F";
+                humidityElement.innerHTML = "Humidity: " + action.data.main.humidity + "%";
+                windElement.innerHTML = "Wind Speed: " + action.data.wind.speed + " MPH";
+                var lat = action.data.coord.lat;
+                var lon = action.data.coord.lon;
 
-                  var forecastElements = document.querySelectorAll(".forecast");
-                  for (i=0; i<forecastElements.length; i++) {
-                        forecastElements[i].innerHTML = "";
-                      
-                      var forecastIndex = i*8 + 4;
-                      var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
-                      var forecastDay = forecastDate.getDate();
-                      var forecastMonth = forecastDate.getMonth() + 1;
-                      var forecastYear = forecastDate.getFullYear();
-                      var forecastDateEl = document.createElement("p");
-                      
-                      forecastDateEl.setAttribute("class","mt-3 mb-0 forecast-date");
-                      forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-                      forecastElements[i].append(forecastDateEl);
-                      
-                      var forecastWeatherElement = document.createElement("img");
-                      forecastWeatherElement.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
-                      forecastWeatherElement.setAttribute("alt",response.data.list[forecastIndex].weather[0].description);
-                      forecastElements[i].append(forecastWeatherElement);
-                      
-                      var forecastTempElement = document.createElement("p");
-                      forecastTempElement.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
-                      forecastElements[i].append(forecastTempElement);
-                      
-                      var forecastHumidityElement = document.createElement("p");
-                      forecastHumidityElement.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
-                      forecastElements[i].append(forecastHumidityElement);
-                      }
-                  })
-              });  
-          }
+                var UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+                axios.get(UVQueryURL)
+                    .then(function (action) {
+                        var UVIndex = document.createElement("span");
+                        UVIndex.setAttribute("class", "badge badge-danger");
+                        UVIndex.innerHTML = action.data[0].value;
+                        uvIndexElement.innerHTML = "UV Index: ";
+                        uvIndexElement.append(UVIndex);
+                    });
 
-          searchElement.addEventListener("click",function() {
+                var cityID = response.data.id;
+                var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+                axios.get(forecastQueryURL)
+                    .then(function (action) {
+
+                        console.log(action);
+
+                        var forecastElements = document.querySelectorAll(".forecast");
+                        for (i = 0; i < forecastElements.length; i++) {
+                            forecastElements[i].innerHTML = "";
+
+                            var forecastIndex = i * 8 + 4;
+                            var forecastDate = new Date(action.data.list[forecastIndex].dt * 1000);
+                            var forecastDay = forecastDate.getDate();
+                            var forecastMonth = forecastDate.getMonth() + 1;
+                            var forecastYear = forecastDate.getFullYear();
+                            var forecastDateEl = document.createElement("p");
+
+                            forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                            forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                            forecastElements[i].append(forecastDateEl);
+
+                            var forecastWeatherElement = document.createElement("img");
+                            forecastWeatherElement.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                            forecastWeatherElement.setAttribute("alt", action.data.list[forecastIndex].weather[0].description);
+                            forecastElements[i].append(forecastWeatherElement);
+
+                            var forecastTempElement = document.createElement("p");
+                            forecastTempElement.innerHTML = "Temp: " + k2f(action.data.list[forecastIndex].main.temp) + " &#176F";
+                            forecastElements[i].append(forecastTempElement);
+
+                            var forecastHumidityElement = document.createElement("p");
+                            forecastHumidityElement.innerHTML = "Humidity: " + action.data.list[forecastIndex].main.humidity + "%";
+                            forecastElements[i].append(forecastHumidityElement);
+                        }
+                    })
+            });
+    }
+
+    searchElement.addEventListener("click", function () {
+        var searchTerm = inputElement.value;
+        getWeather(searchTerm);
+        searchHistory.push(searchTerm);
+        localStorage.setItem("search", JSON.stringify(searchHistory));
+        renderSearchHistory();
+    })
+    inputElement.addEventListener("keydown", function (event) {
+        if (event.which === 13) {
+
+
+
             var searchTerm = inputElement.value;
             getWeather(searchTerm);
             searchHistory.push(searchTerm);
-            localStorage.setItem("search",JSON.stringify(searchHistory));
-            renderSearchHistory();
-        })
-        inputElement.addEventListener("keydown",function(event){
-            if (event.which === 13){
-                
-
-          
-            var searchTerm = inputElement.value;
-            getWeather(searchTerm);
-            searchHistory.push(searchTerm);
-            localStorage.setItem("search",JSON.stringify(searchHistory));
+            localStorage.setItem("search", JSON.stringify(searchHistory));
             renderSearchHistory();
         }
-        });
-  
-        clearElement.addEventListener("click",function() {
-            searchHistory = [];
-            renderSearchHistory();
-        })
-    function clearHistory(){
-    clearHistory = (localStorage.clear("clear-history"))
-}
-    clearHistory()
-    
-        function k2f(K) {
-        return Math.floor((K - 273.15) *1.8 +32);
+    });
+
+    clearElement.addEventListener("click", function () {
+        searchHistory = [];
+        renderSearchHistory();
+    })
+    function clearHistory() {
+        clearHistory = (localStorage.clear("clear-history"))
     }
-  
+    clearHistory()
+
+    function k2f(K) {
+        return Math.floor((K - 273.15) * 1.8 + 32);
+    }
+
     function renderSearchHistory() {
         historyElement.innerHTML = "";
-        for (var i=0; i<searchHistory.length; i++) {
+        for (var i = 0; i < searchHistory.length; i++) {
             var historyItem = document.createElement("input");
-   
-            historyItem.setAttribute("type","text");
-            historyItem.setAttribute("readonly",true);
+
+            historyItem.setAttribute("type", "text");
+            historyItem.setAttribute("readonly", true);
             historyItem.setAttribute("class", "form-control d-block bg-white");
             historyItem.setAttribute("value", searchHistory[i]);
-            historyItem.addEventListener("click",function() {
+            historyItem.addEventListener("click", function () {
                 getWeather(historyItem.value);
             })
             historyElement.append(historyItem);
         }
     }
-  
+
     renderSearchHistory();
     if (searchHistory.length > 0) {
         getWeather(searchHistory[searchHistory.length - 1]);
     }
-  
 
 
-} 
-  renderPage();
+
+}
+renderPage();
